8467926d4f7bd87ea1ff92927ade1fb7fcdbade1
yumanakayama <nakayama.yuma@servithink.co.jp>
2024-03-28 16:27:42 +0900

    test

--- lib/client.ts
+++ lib/client.ts
@@ -5,15 +5,15 @@ const client = createClient({
   apiKey: process.env.NEXT_PUBLIC_API_KEY || "",
 
   // 以下、microcmsの強制キャッシュクリア
-  customFetch: (input: any, init: any) => {
-    if (typeof input === 'string') {
-      const newInput = new URL(input)
-      const time = new Date()
-      newInput.searchParams.set('cacheclearparam', `${time.getMinutes()}`)
-      return fetch(newInput.href, init)
-    }
-    return fetch(input, init)
-  },
+  // customFetch: (input: any, init: any) => {
+  //   if (typeof input === 'string') {
+  //     const newInput = new URL(input)
+  //     const time = new Date()
+  //     newInput.searchParams.set('cacheclearparam', `${time.getMinutes()}`)
+  //     return fetch(newInput.href, init)
+  //   }
+  //   return fetch(input, init)
+  // },
 
 });
 
